bf6caf6287f5fd10a2429aedd0108814ce2f57ff
spidomisiek88 <michal.pajak88@gmail.com>
2019-07-11 12:21:33 +0200

    first commit

---
+++ src/main/java/pl/michalPajak/vehiclesTask/VehiclesTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.michalPajak.vehiclesTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesTaskApplication.class, args);
+	}
+
+}
